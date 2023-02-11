187e09c272f380afa4288c6b08c008fcdc904dac
Enrique Martín Garay <98557235+Quique40@users.noreply.github.com>
2023-02-11 10:09:46 -0300

    Added routes for operations in transaction table

--- src/controllers/transaccion.js
+++ src/controllers/transaccion.js
@@ -1,4 +1,10 @@
-const {Transaccion, Variable, Vendedor, Cliente, Inventario} = require("../db");
+const {
+  Transaccion,
+  Variable,
+  Vendedor,
+  Cliente,
+  Inventario,
+} = require('../db');
 
 const createTransaction = async (req, res) => {
   try {
@@ -56,6 +62,53 @@ const getNumberOnder = async (req, res) => {
   }
 };
 
+const getOrder = async (req, res) => {
+  try {
+    const allOrder = await Transaccion.findAll();
+    const order = allOrder.map((e) => {
+      return {
+        vendedorId: e.vendedorId,
+        clienteId: e.clienteId,
+        inventarioId: e.inventarioId,
+        descripcion: e.descripcion,
+        cantidad: e.cantidad,
+        orderNumber: e.orderNumber,
+      };
+    });
+    res.status(200).json(order);
+  } catch (e) {
+    res.status(400).send(e);
+  }
+};
+
+const getOrderByNumber = async (req, res) => {
+  try {
+    let { numberOrder } = req.params;
+    const order = await Transaccion.findAll({
+      where: {
+        orderNumber: numberOrder,
+      },
+    });
+    res.status(200).json(order);
+  } catch (e) {
+    res.status(400).send(e);
+  }
+};
+
+const getOrderById = async (req, res) => {
+  try {
+    let { id } = req.params;
+    const order = await Transaccion.findAll({
+      where: {
+        id: id,
+      },
+    });
+    res.status(200).json(order);
+  } catch (e) {
+    res.status(400).send(e);
+  }
+};
+
 const putNumberOrder = async (req, res) => {
   try {
     const newNumber = req.params.number;
@@ -68,9 +121,48 @@ const putNumberOrder = async (req, res) => {
   }
 };
 
+const putTransaction = async (req, res) => {
+  try {
+    const {
+      id,
+      clienteId,
+      inventarioId,
+      descripcion,
+      costo,
+      cantidad,
+      subTotal,
+      fecha,
+      observacion,
+      orderNumber,
+    } = req.body;
+
+    const order = await Transaccion.findByPk(id);
+
+    order.clienteId = clienteId;
+    order.inventarioId = inventarioId;
+    order.descripcion = descripcion;
+    order.costo = costo;
+    order.cantidad = cantidad;
+    order.subTotal = subTotal;
+    order.fecha = fecha;
+    order.observacion = observacion;
+    order.orderNumber = orderNumber;
+
+    order.save();
+
+    res.status(200).json(order);
+  } catch (error) {
+    res.status(404).send(error);
+  }
+};
+
 module.exports = {
   createTransaction,
   getNumberOnder,
+  getOrder,
+  getOrderByNumber,
+  getOrderById,
   putNumberOrder,
+  putTransaction,
   createVariable,
 };
